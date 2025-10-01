0f8a38b85f0afe2718a9241ee0fc2575c8d14e98
Abodi248 <74561089+Abodi248@users.noreply.github.com>
2025-10-01 21:24:53 +1000

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+// eslint.config.js
+import { defineConfig } from "eslint-define-config";
+
+export default defineConfig({
+  root: true,
+  env: {
+    node: true,
+    es2022: true,
+    jest: true
+  },
+  extends: [
+    "eslint:recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module"
+  },
+  rules: {
+    "no-unused-vars": "warn",
+    "no-console": "off",
+    "semi": ["error", "always"]
+  }
+});
