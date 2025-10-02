f9b8df666f6449b7e054a558a49dc463144576b6
Abodi248 <74561089+Abodi248@users.noreply.github.com>
2025-10-02 23:06:15 +1000

    Update and rename server.js to server.cjs

--- server.cjs
+++ server.cjs
@@ -1,5 +1,5 @@
 
-import app from './index.js';
+import app from './index.cjs';
 const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
